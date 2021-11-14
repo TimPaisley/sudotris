5b460fc486eda087be5e2a750a44525d0727e50d
Tim Paisley <tim.paisley@wcc.govt.nz>
2021-11-15 11:13:57 +1300

    Add different variations of shapes and choose one at random

--- src/Shape/Shape.tsx
+++ src/Shape/Shape.tsx
@@ -1,18 +1,13 @@
 import styles from './Shape.module.css'
 import classNames from 'classnames'
+import shapes from './shapes'
 
 export default function Shape() {
-  const structure = [
-    [ 0, 0, 0, 0, 0 ],
-    [ 0, 0, 1, 0, 0 ],
-    [ 0, 1, 1, 1, 0 ],
-    [ 0, 0, 1, 0, 0 ],
-    [ 0, 0, 0, 0, 0 ],
-  ]
+  const randomShape = shapes[Math.floor(Math.random() * shapes.length)]
 
   return (
     <div className={styles.shape}>
-      {structure.map((line, i) => (
+      {randomShape.map((line, i) => (
         <div key={i} className={styles.line}>
           {line.map((tile, i) => (
             <div key={i} className={classNames(styles.tile, tile && styles.active)} />
